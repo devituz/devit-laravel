2a04b479726431e768e7f31f0cc7296d6d213812
Shohbozbek Turg'unov (Devit) <shohbozbek.uz24@gmail.com>
2025-04-16 19:49:51 +0500

    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to Devituz</title>
+    <title>{{ env('APP_NAME') }}</title>
     <link rel="stylesheet" href="{{asset('assets/css/devituz.css')}}">
 
 </head>
